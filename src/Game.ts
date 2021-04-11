d6d5fcbfd5c2b3ad1291523abf2b60c9bcbb0836
Benjamin Møller Jensen <bemoje@gmail.com>
2021-04-11 20:08:37 +0200

    implicitly public class methods explicitly defined as public
    Cosmetic code changes only

--- src/Game.ts
+++ src/Game.ts
@@ -5,12 +5,12 @@ import type { Piece } from './AbstractPiece';
 import type { Position } from './Position';
 
 export class Game {
-  board: Board;
-  white: Player;
-  black: Player;
-  moves: Array<Move>;
+  public board: Board;
+  public white: Player;
+  public black: Player;
+  public moves: Array<Move>;
 
-  constructor() {
+  public constructor() {
     this.board = new Board(this);
     this.white = new Player(this, 'white');
     this.black = new Player(this, 'black');
@@ -21,7 +21,7 @@ export class Game {
    * Iterate each piece on the board.
    * If the callback function returns true, iteration ends.
    */
-  forEachPiece(f: (piece: Piece) => boolean | void): void {
+  public forEachPiece(f: (piece: Piece) => boolean | void): void {
     const w = this.white.pieces;
     const b = this.black.pieces;
     for (let i = 0; i < 16; i++) {
@@ -37,7 +37,7 @@ export class Game {
    * Allows for skipping validation of the move's legality according to the rules of the game. This is used internally
    * for performance reasons when cloning the game, repeating the moves that were previously checked.
    */
-  makeMove(piece: Piece, to: Position, skipValidation?: boolean): void {
+  public makeMove(piece: Piece, to: Position, skipValidation?: boolean): void {
     if (skipValidation || piece.isValidMovePosition(to)) {
       const targetPiece = piece.game.board.getPieceByPosition(to);
       const move = new Move(piece, to, targetPiece);
@@ -53,7 +53,7 @@ export class Game {
   /**
    * Returns a deep clone of the game instance.
    */
-  clone(): Game {
+  public clone(): Game {
     const game = new Game();
     const w = game.white.pieces;
     const b = game.black.pieces;
